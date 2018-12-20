878a2b723167d260743c5cb98d713ade9c7d8ab4
Patrick Wilkes <30908698+patrickwilkes82@users.noreply.github.com>
2018-12-20 12:51:05 -0500

    Create Connect_to_O365.ps1

---
+++ Connect_to_O365.ps1
@@ -0,0 +1,3 @@
+$Creds = Get-Credential "admin.onepath@g100companies.com"
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Creds -Authentication Basic –AllowRedirection
+Import-PSSession $Session
