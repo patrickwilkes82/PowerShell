87faa12910eee38a5969af4aa6efe25cf5799464
patrickwilkes82 <patwilkes82@gmail.com>
2018-12-28 12:24:28 -0500

    Added Mailbox Access Script

---
+++ Office 365 & Exchange/Access to Mailbox Folders.ps1
@@ -0,0 +1,22 @@
+# ***CONNECT TO OFFICE 365***
+$Creds = Get-Credential "admin.onepath@g100companies.com"
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Creds -Authentication Basic –AllowRedirection
+Import-PSSession $Session
+
+# ***VARIABLES
+$mailbox = 'user1@domain.com' #Email address of user mailbox [Add ':\Calendar' or ':\Contacts' to grant access to specific mailbox folders]
+$user = 'user2@domain.com' #Email of users mailbox being granted access 
+$rights = 'Editor' #Permissions to folder [FullAccess, Editor, ReadOnly, etc...]
+
+# ***GET Current Permissions to mailbox
+Get-MailboxFolderPermission -Identity $mailbox -user $user
+
+# ***ADD Permissions if none exists***
+Add-MailboxFolderPermission -Identity $mailbox -user $user -AccessRights $rights
+
+# ***SET Permissions if other permission already exists
+Set-MailboxFolderPermission -Identity $mailbox -user $user -AccessRights $rights
+
+
+# ***DELETE Permissions to mailbox
+Remove-MailboxFolderPermission -Identity $mailbox -user $user 
