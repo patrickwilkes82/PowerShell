5049af34f123b020e1b4e6d7343de78b53918ff1
Onepath Admin <admin.onepath@g100companies.com>
2019-04-29 11:57:46 -0400

    Minor Updates

--- Office 365 & Exchange/Create Shared Mailbox.ps1
+++ Office 365 & Exchange/Create Shared Mailbox.ps1
@@ -12,7 +12,7 @@ $user = Read-Host 'Enter Email Address of User Gaining Access to Mailbox'
 try {
 New-Mailbox -Shared -Name $name -DisplayName $name -PrimarySmtpAddress $mailbox
 Write-Host 'Shared Mailbox $mailbox Created Successfully'
-Add-MailboxPermission -Identity $mailbox -User $user -AccessRights FullAccess -InheritenceType All -AutoMapping:$true
+Add-MailboxPermission -Identity $mailbox -User $user -AccessRights FullAccess
 Write-Host '$user Granted FullAccess Permissions to Shared Mailbox $mailbox'
 Add-RecipientPermission -Identity $mailbox -Trustee $user -AccessRights SendAs
 Write-Host '$user Granted SendAs Permission to Shared Mailbox $mailbox'
@@ -20,5 +20,6 @@ Write-Host '$user Granted SendAs Permission to Shared Mailbox $mailbox'
 catch {
     Write-Host 'Error Occurred please check for any spelling errors'
 }
-
+Get-PSSession | Remove-PSSession
+Write-Host 'PS Session Removed'
 Pause
