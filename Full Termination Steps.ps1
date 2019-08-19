9355c95290f28c596851b6f3d392585ea6d21969
Onepath Admin <admin.onepath@g100companies.com>
2019-08-19 14:12:42 -0400

    New script

--- Full Termination Steps.ps1
+++ Full Termination Steps.ps1
@@ -1,21 +1,17 @@
-# --VARIABLES--
-$O365Creds = Get-Credential "admin.onepath@g100companies.com"
-$user = Read-Host "Enter Username"
-$mailbox = Read-Host "Enter Email Address"
-
 # --Connect to Office 365--
 
 Write-Host "Attempting to Connect to Office 365" -ForegroundColor Yellow -BackgroundColor DarkGray
-
+$Creds = Get-Credential "admin.onepath@g100companies.com"
 
 try {
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $O365Creds -Authentication Basic -AllowRedirection
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Creds -Authentication Basic -AllowRedirection
 Import-PSSession $Session
 Write-Host "Connected to Office 365" -ForegroundColor Green -BackgroundColor Gray
 }
 catch {
-    Write-Host 'Unable to Connect to Office 365' -ForegroundColor Red -BackgroundColor DarkGray
+    Write-Host 'Unable to Connect to Office 365. Please Check for any Errors' -ForegroundColor Red -BackgroundColor DarkGray
+    Pause
 }
 
-Pause
+Write-Host "Ready to Continue"
 Exit
