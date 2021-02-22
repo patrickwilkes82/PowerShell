c89598d9f86ba00ab66c6f9aa08ed690a290ce88
Patrick Wilkes <30908698+patrickwilkes82@users.noreply.github.com>
2021-02-22 10:36:51 -0500

    Update Connect_to_O365.ps1

--- Office 365 & Exchange/Connect_to_O365.ps1
+++ Office 365 & Exchange/Connect_to_O365.ps1
@@ -1,3 +1,3 @@
-$Creds = Get-Credential "admin.onepath@g100companies.com"
+$Creds = Get-Credential "admin.onepath@counciladvisors.com"
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Creds -Authentication Basic -AllowRedirection
 Import-PSSession $Session
